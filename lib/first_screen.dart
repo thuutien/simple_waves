342b09e18dca913121ab9255e5fa73dad8e925b1
Huu Tran <trantien.sm@gmail.com>
2020-07-09 21:28:24 -0500

    update1

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,13 +1,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:simple_waves/animated_backgroud.dart';
 import 'package:simple_waves/animated_wave.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Expanded(
@@ -16,8 +16,52 @@ class FirstScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('user name'),
-                Text('password'),
+                Icon(
+                  Icons.play_for_work,
+                  size: 100,
+                  color: Color(0xff1dd3bd),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'username',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Color(0xff1dd3bd),
+                  shape: StadiumBorder(),
+                  onPressed: () {},
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    width: 50,
+                    height: 50,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Need help?',
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                ),
+                SizedBox(height: 100)
               ],
             ),
           )),
@@ -28,7 +72,15 @@ class FirstScreen extends StatelessWidget {
                 child: AnimatedWave(
                   color: Color(0xff2bb2bb),
                   height: 120,
+                  speed: 0.7,
+                ),
+              ),
+              Container(
+                child: AnimatedWave(
+                  color: Color(0xff87d4c5),
+                  height: 100,
                   speed: 1.0,
+                  offset: pi,
                 ),
               ),
               Container(
@@ -36,14 +88,6 @@ class FirstScreen extends StatelessWidget {
                   color: Color(0xff1dd3bd),
                   height: 100,
                   speed: 1.4,
-                  offset: pi,
-                ),
-              ),
-              Container(
-                child: AnimatedWave(
-                  color: Color(0xff87d4c5),
-                  height: 100,
-                  speed: 0.7,
                   offset: pi / 2,
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class AnimationDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Color(0xff1dd3bd)),
       home: FirstScreen(),
     );
   }
